e08029ce112704ea897c5107cc150c39f43f5a2b
Miguel Sesma <miguel.sesma@touchsurgery.com>
2018-10-01 14:05:57 +0100

    Use coroutines library V0.30.0

--- app/src/test/java/eu/sesma/kuantum/CoroutinePracticeUnitTest.kt
+++ app/src/test/java/eu/sesma/kuantum/CoroutinePracticeUnitTest.kt
@@ -113,34 +113,33 @@ class CoroutinePracticeUnitTest {
         job.join()
     }
 
-
-//    @Test
-//    fun test6 () = runBlocking {
-//        val supervisor = SupervisorJob()
-//        with(CoroutineScope(coroutineContext + supervisor)) {
-//            // launch the first child -- its exception is ignored for this example (don't do this in practise!)
-//            val firstChild = launch(CoroutineExceptionHandler { _, _ ->  }) {
-//                println("First child is failing")
-//                throw AssertionError("First child is cancelled")
-//            }
-//            // launch the second child
-//            val secondChild = launch {
-//                firstChild.join()
-//                // Cancellation of the first child is not propagated to the second child
-//                println("First child is cancelled: ${firstChild.isCancelled}, but second one is still active")
-//                try {
-//                    delay(Long.MAX_VALUE)
-//                } finally {
-//                    // But cancellation of the supervisor is propagated
-//                    println("Second child is cancelled because supervisor is cancelled")
-//                }
-//            }
-//            // wait until the first child fails & completes
-//            firstChild.join()
-//            println("Cancelling supervisor")
-//            supervisor.cancel()
-//            secondChild.join()
-//        }
-//    }
+    @Test
+    fun test6 () = runBlocking {
+        val supervisor = SupervisorJob()
+        with(CoroutineScope(coroutineContext + supervisor)) {
+            // launch the first child -- its exception is ignored for this example (don't do this in practise!)
+            val firstChild = launch(CoroutineExceptionHandler { _, _ ->  }) {
+                println("First child is failing")
+                throw AssertionError("First child is cancelled")
+            }
+            // launch the second child
+            val secondChild = launch {
+                firstChild.join()
+                // Cancellation of the first child is not propagated to the second child
+                println("First child is cancelled: ${firstChild.isCancelled}, but second one is still active")
+                try {
+                    delay(Long.MAX_VALUE)
+                } finally {
+                    // But cancellation of the supervisor is propagated
+                    println("Second child is cancelled because supervisor is cancelled")
+                }
+            }
+            // wait until the first child fails & completes
+            firstChild.join()
+            println("Cancelling supervisor")
+            supervisor.cancel()
+            secondChild.join()
+        }
+    }
 
 }
